29c46a5f9e99bbef2f17449a381d6e715f7cc861
Younes Lahouiti <loller763@gmail.com>
2022-11-11 11:31:02 +0100

    Débug des notes de frais qui n'étaient pas dans l'ordre si la date n'était pas en Français

--- src/containers/NewBill.new.js
+++ src/containers/NewBill.new.js
@@ -10,34 +10,43 @@ export default class NewBill {
       `form[data-testid="form-new-bill"]`
     );
     formNewBill.addEventListener("submit", this.handleSubmit);
-    const file = this.document.querySelector(`input[data-testid="file"]`);
-
-    file.addEventListener("change", this.handleChangeFile);
+    const fileInput = this.document.querySelector(`input[data-testid="file"]`);
+    fileInput.addEventListener("change", this.handleChangeFile);
+    //
+    this.file = null;
     this.fileUrl = null;
     this.fileName = null;
     this.billId = null;
+    //
     new Logout({ document, localStorage, onNavigate });
   }
+
   handleChangeFile = (e) => {
     e.preventDefault();
-
     const inputFile = this.document.querySelector(`input[data-testid="file"]`);
-
-    // inputFile.createAttribute("accept");
-    inputFile.setAttribute("accept", "image/*,.png,.jpg,.jpeg");
 
     const file = inputFile.files[0];
 
+    //If no file was added
     if (!file) {
       console.log("No file was added");
       return;
     }
 
+    this.file = file;
+    console.log(
+      "%cthis.file:",
+      "background: darkblue; padding: 5px;",
+      this.file
+    );
+
+    //To check if the file sent is an image and the format of the image is acceptable
     let fileIsAnImageWithAcceptableFormat =
       file.type.includes("image/png") ||
       file.type.includes("image/jpg") ||
       file.type.includes("image/jpeg");
-    if (file && fileIsAnImageWithAcceptableFormat) {
+
+    if (fileIsAnImageWithAcceptableFormat) {
       console.log(
         "%cFile type is an image with an acceptable format",
         "font-size:20px; padding: 5px; background: green"
@@ -53,97 +62,113 @@ export default class NewBill {
     const filePath = e.target.value.split(/\\/g);
     const fileName = filePath[filePath.length - 1];
 
-    this.fileName = fileName;
-    this.fileUrl = e.target.value;
+    const email = JSON.parse(localStorage.getItem("user")).email;
 
     const formData = new FormData();
-    formData.append("fileUrl", this.fileUrl);
-    formData.append("fileName", this.fileName);
-
-    //There are errors after line 58 (if we're in line 48) since we append the form data and send just: email and file
-    //WITHOUT any other info about the form itself such as the
-    console.log(this.fileUrl, this.fileName);
+    formData.append("email", email);
+    formData.append("file", file);
 
     this.store
       .bills()
       .create({
         data: formData,
         headers: {
-          noContentType: false,
+          noContentType: true,
         },
       })
       .then(({ fileUrl, key }) => {
-        console.log({ fileUrl, key });
+        console.log(fileUrl);
         this.billId = key;
         this.fileUrl = fileUrl;
         this.fileName = fileName;
-        e.preventDefault();
       })
       .catch((error) => console.error(error));
   };
 
+  //
   handleSubmit = (e) => {
     e.preventDefault();
 
     const userInfos = JSON.parse(localStorage.getItem("user"));
+
     const valueOfEmail = userInfos.email;
 
     const valueOfType = e.target.querySelector(
       `select[data-testid="expense-type"]`
     ).value;
+
     const valueOfName = e.target.querySelector(
       `input[data-testid="expense-name"]`
     ).value;
+
     const valueOfAmount = e.target.querySelector(
       `input[data-testid="amount"]`
     ).valueAsNumber;
+
     const valueOfDate = e.target.querySelector(
       `input[data-testid="datepicker"]`
     ).valueAsDate;
+
     const valueOfPercentage =
       e.target.querySelector(`input[data-testid="pct"]`).valueAsNumber || 20;
     const valueOfVAT = e.target.querySelector(
       `input[data-testid="vat"]`
     ).valueAsNumber;
+
     const valueOfCommentary = e.target.querySelector(
       `textarea[data-testid="commentary"]`
     ).value;
 
-    /* 
-    const email = JSON.parse(localStorage.getItem("user")).email
     const bill = {
-      email,
-      type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
-      amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
-      vat: e.target.querySelector(`input[data-testid="vat"]`).value,
-      pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
-      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
+      email: valueOfEmail,
+      type: valueOfType,
+      name: valueOfName,
+      amount: valueOfAmount,
+      date: valueOfDate,
+      vat: valueOfVAT,
+      pct: valueOfPercentage,
+      commentary: valueOfCommentary,
       fileUrl: this.fileUrl,
       fileName: this.fileName,
-      status: 'pending'
+      status: "pending",
+    };
+
+    let propertiesAreDefined = false;
+    let counterOfDefinedProperties = 0;
+
+    for (const property in bill) {
+      console.log(property, "has a value of → ", bill[property]);
+
+      const propertyIsNotUndefined = !!bill[property];
+      if (propertyIsNotUndefined) {
+        counterOfDefinedProperties++;
+        continue;
+      } else {
+        break;
+      }
     }
-    this.updateBill(bill)
-    this.onNavigate(ROUTES_PATH['Bills'])
-  }
-*/
 
-    let bill = [];
-    for (let pair of formData.entries()) {
-      console.log(pair[0] + ", " + pair[1]);
-      bill.push([pair[0], pair[1]]);
+    const amountOfPropertiesInBill = Object.keys(bill).length;
+
+    //Verifies if all the propertied are defined
+    counterOfDefinedProperties === amountOfPropertiesInBill
+      ? (propertiesAreDefined = true)
+      : (propertiesAreDefined = false);
+
+    //Depending on whether all the properties in the bill object are defined or not, the bill will or won't be sent to the Back-end
+    if (propertiesAreDefined) {
+      console.log({ propertiesAreDefined }, "✔");
+      this.updateBill(bill);
+      this.onNavigate(ROUTES_PATH["Bills"]);
+    } else {
+      console.log({ propertiesAreDefined }, "❌");
+      return;
     }
-
-    bill = Object.fromEntries(bill);
-    console.log(bill);
-
-    this.updateBill(bill);
-    this.onNavigate(ROUTES_PATH["Bills"]);
   };
 
   // not need to cover this function by tests
   updateBill = (bill) => {
+    console.log("this.store:\n", this.store);
     if (this.store) {
       this.store
         .bills()
